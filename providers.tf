4bbbb0a70d53f6f003fd5ddb4a6a17e5b6ee45ee
nicolaka <nicolaka@gmail.com>
2023-11-15 00:19:11 +0000

    initial commit

---
+++ providers.tf
@@ -0,0 +1,35 @@
+terraform {
+ required_version = "~> 1.5"
+ required_providers {
+        vault = {
+            source  = "hashicorp/vault"
+            version =  "~> 3.19.0"
+        }
+        helm = {
+            source  = "hashicorp/helm"
+            version = "~> 2.10.1"
+        }
+        kubernetes = {
+            source  = "hashicorp/kubernetes"
+            version = "~> 2.22.0"
+        }
+    }
+}
+
+provider "vault" {
+  address = var.vault_public_address
+  namespace = var.vault_namespace
+  token = var.vault_admin_token
+} 
+
+provider "kubernetes" {
+  config_path           = "~/.kube/config"
+  config_context = "arn:aws:eks:us-east-2:713653441474:cluster/astro-us-east-2-eks"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+    config_context = "arn:aws:eks:us-east-2:713653441474:cluster/astro-us-east-2-eks"
+  }
+}
